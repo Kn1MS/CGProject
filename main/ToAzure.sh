6b6a87023e73737b2c2b61e82e5f1966d461bf88
Kn1MS <sensor-big@inbox.ru>
2019-12-17 05:10:16 +0300

    ssh user fix

--- main/ToAzure.sh
+++ main/ToAzure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 az vm start --name MyVm --resource-group MyResourceGroup
-ssh azureuser@137.135.241.56 python3 CGProject/GenerateNumber.py
+ssh kn1ms@137.135.241.56 python3 CGProject/GenerateNumber.py
 az vm deallocate --resource-group MyResourceGroup --name MyVm
