ce42d548c4d6234b05620b85924a8a3344c3cd3a
Kn1MS <sensor-big@inbox.ru>
2019-12-17 03:02:52 +0300

    Change folder for ssh run

--- main/ToAzure.sh
+++ main/ToAzure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 az vm start --name MyVm --resource-group MyResourceGroup
-ssh azureuser@137.135.241.56 python3 RandomNumberGenerator/GenerateNumber.py
+ssh azureuser@137.135.241.56 python3 CGProject/GenerateNumber.py
 az vm deallocate --resource-group MyResourceGroup --name MyVm
