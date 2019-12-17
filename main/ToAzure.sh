7ca101e3ad080dcc2e732c5c3071a02aacb4ebe0
Kn1MS <sensor-big@inbox.ru>
2019-12-17 12:42:06 +0300

    Fix connection

--- main/ToAzure.sh
+++ main/ToAzure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 az vm start --name MyVm --resource-group MyResourceGroup
 sleep 60s
-ssh kn1ms@13.94.101.35 python3 CGProject/GenerateNumber.py
+ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/id_rsa kn1ms@13.94.101.35 python3 CGProject/GenerateNumber.py
 az vm deallocate --resource-group MyResourceGroup --name MyVm
