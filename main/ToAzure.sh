c15541c478c8bf67898973e9f4c81ffa413da988
Kn1MS <sensor-big@inbox.ru>
2019-12-17 05:58:41 +0300

    Script fix

--- main/ToAzure.sh
+++ main/ToAzure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 az vm start --name MyVm --resource-group MyResourceGroup
 ssh -tt kn1ms@40.127.98.194 python3 CGProject/GenerateNumber.py
+sleep 10s
 az vm deallocate --resource-group MyResourceGroup --name MyVm
