d6392de768fadf7fada6ee6bed2681a6e8f6fae8
Kn1MS <sensor-big@inbox.ru>
2019-12-17 05:20:12 +0300

    ip fix

--- main/ToAzure.sh
+++ main/ToAzure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 az vm start --name MyVm --resource-group MyResourceGroup
-ssh kn1ms@137.135.241.56 python3 CGProject/GenerateNumber.py
+ssh kn1ms@40.127.98.194 python3 CGProject/GenerateNumber.py
 az vm deallocate --resource-group MyResourceGroup --name MyVm
